7cd0cf80cf8ffafd74798c7a12f30668114d1e05
Dominik Boboš <bobos.dominik@gmail.com>
2020-02-20 08:54:45 +0100

    nothing really matters

--- interpret.py
+++ interpret.py
@@ -1,18 +1,24 @@
 import xml.etree.ElementTree as elemTree
 import argparse as argumentParser
 import sys
+import os
 
-#aby sa predoslo pouzivaniu vstavanej chybovej hlasky z argparse
+
+"""
+	ARGS PARSING SECTION
+"""
+#to override built in error message from argparse
 class ArgumentParser(argumentParser.ArgumentParser):	
 	def error(self, message):
-		sys.stderr.write("Chyba: Použité nesprávne argumenty!\n")
+		sys.stderr.write("ERROR: Used wrong arguments! Use '--help' for program manual.\n")
 		exit(10)
 
-# pridanie ostatných prípustných argumentov
+# adding possible options 
 parser = ArgumentParser(add_help=False)
 parser.add_argument("--help", action="store_true")
-parser.add_argument("--source",action="store_true")
-parser.add_argument("--stats",action="store_true")
+parser.add_argument("--source", metavar="FILE", type=str)
+parser.add_argument("--input",  metavar="FILE", type=str)
+parser.add_argument("--stats",action="store_true")			#metavar='FILE'	type=str
 parser.add_argument("--insts", action="store_true")
 parser.add_argument("--vars", action="store_true")
 
@@ -20,20 +26,86 @@ arguments = parser.parse_args()
 
 if arguments.help:
 	if len(sys.argv) != 2:
-		sys.stderr.write("CHYBA: argument '--help' je potrebné použiť samostatne!\n")
+		sys.stderr.write("ERROR: '--help' need to be used standalone.\n")
 		exit(10)
 	else:
-		print("POMOC SO SKRIPTOM")
+		print("POMOC SO SKRIPTOM takze tu treba este dopisat stracky na pomoc")
 		exit(0)
 
-for x in range(1,len(sys.argv)):
-	if not arguments.source or not arguments.stats:
-		sys.stderr.write("CHYBA, nie je zadaný parameter '--source' alebo '--stats'!\n")
-		exit(10)
+if not arguments.source and not arguments.input:
+	sys.stderr.write("ERROR: use at least one argument '--source' or '--input'!\n")
+	exit(10)
+else:
+	if arguments.source and not os.path.isfile(arguments.source):
+		sys.stderr.write("ERROR: Cannot open input file set by --source.\n")
+		exit(11)
+	if arguments.input and not os.path.isfile(arguments.input):
+		sys.stderr.write("ERROR: Cannot open input file set by --input.\n")
+		exit(11)
 
+if (arguments.insts or arguments.vars) and not arguments.stats:
+	sys.stderr.write("ERORR: '--insts' or '--vars' used without '--stats'!\n")
+	exit(10)
+
+#TOTO SA BUDE MUSIET ASI PRIDAT NAKONIEC
+for x in range(1,len(sys.argv)):
+	
 	if (sys.argv[x] == '--insts'):
-		print('--insts accepted')	#vypise sa do suboru
+		print('--insts accepted')	#prints to file
 	elif (sys.argv[x] == '--vars'):
-		print('--vars accepted')	#vypise sa do suboru
+		print('--vars accepted')	#prints to file
+"""
+END OF ARGS PARSING SECTION
+"""
+
+
+class XMLparser:
+	def __init__(self, input_xml, stack): #, frames, flowControl):
+		self.__instructions = None
+		self.__stack = stack
+		#self.__frames = frames
+		#self.__flowControl = flowControl  # Instruction counter and labelstack
+
+				# Start parsing source XML
+		try:
+			tree = elemTree.parse(args.source)
+			# Valid root element
+			root = tree.getroot()
+			if root.tag != "program":
+				raise elemTree.ParseError("root element must be 'program'")
+
+			if "language" not in root.attrib:
+				raise elemTree.ParseError("missing attribute 'language'")
+
+			for attrib, value in root.attrib.items():
+				if attrib == "language":
+					if value != "IPPcode20":
+						raise elemTree.ParseError("language must be 'IPPcode20' ")
+				elif attrib != "name" and attrib != "description":
+					raise elemTree.ParseError("program element can only contain language, name or description attributes")
+
+		except elemTree.ParseError as wrongxml:
+			sys.stderr.write("ERROR: wrong XML format (%s)!\n" % str(wrongxml))
+			exit(31)
+
+
+class Interpret:
+	def __init__(self):
+		# STATI extension
+		self.__inst = 0
+		self.__vars = set()
+
+		# Interpret vars
+		self.__instructionList = []
+		#self._flowControl = FlowControl()   # Contains instruction counter, position stack and labels dict
+		self._stack = []
+		#self._frames = Frames()
+
+	def start(self):
+		xml = XMLparser(self.arguments.source, self._stack )	#, self._frames, self._flowControl)
+		print('dostal som sa sem?')
+
+XMLparser.__init__(arguments.source, [] )
+
 
 
---
+++ test.php
@@ -0,0 +1,228 @@
+<?php
+
+declare(strict_types=1);
+
+
+$directory = getcwd();
+$int_file = $directory . "./interpret.py";
+$parse_file = $directory . "./parse.php";
+$jexamxml_file = "/pub/courses/ipp/jexamxml/jexamxml.jar";
+
+$recursive_flag = false;
+$parse_only_flag = false;
+$int_only_flag = false;
+
+/****************************************
+*				  MAIN 	 				*
+****************************************/
+// ak nebol zadaný nijaký prepínač, t.j. vsetko zostane default
+if($argc == 1)
+{
+	;//nejaka funkcia
+}
+else
+{
+    $long_params = array(
+        "help",
+        "directory:",
+        "recursive",
+        "parse-script:",
+        "int-script:",
+        "parse-only",
+        "int-only",
+        "jexamxml:"
+    );
+
+	$params = getopt ( "", $long_params);		//zíkanie argumentov a ich parsovanie do $params
+
+	if (array_key_exists('help', $params) )
+	{
+		if($argc == 2) 							// zadany argument --help
+		{
+            echo <<<EOL
+-Skript (test.php v jazyce PHP 7.4) slúži pre automatické testovanie 
+postupnej aplikácie parse.php a interpret.py
+--help viz společný parametr všech skriptů v sekci 2.2;
+• --directory=path testy bude hledat v zadaném adresáři (chybí-li tento parametr, tak skript
+prochází aktuální adresář; v případě zadání neexistujícího adresáře dojde k chybě 11);
+• --recursive testy bude hledat nejen v zadaném adresáři, ale i rekurzivně ve všech jeho
+podadresářích;
+• --parse-script=file soubor se skriptem v PHP 7.4 pro analýzu zdrojového kódu v IPPcode20 (chybí-li tento parametr, tak implicitní hodnotou je parse.php uložený v aktuálním
+adresáři);
+• --int-script=file soubor se skriptem v Python 3.8 pro interpret XML reprezentace kódu
+v IPPcode20 (chybí-li tento parametr, tak implicitní hodnotou je interpret.py uložený v aktuálním adresáři);
+• --parse-only bude testován pouze skript pro analýzu zdrojového kódu v IPPcode20 (tento
+parametr se nesmí kombinovat s parametry --int-only a --int-script), výstup s referenčním
+výstupem (soubor s příponou out) porovnávejte nástrojem A7Soft JExamXML;
+• --int-only bude testován pouze skript pro interpret XML reprezentace kódu v IPPcode20
+(tento parametr se nesmí kombinovat s parametry --parse-only a --parse-script). Vstupní
+program reprezentován pomocí XML bude v souboru s příponou src.
+• --jexamxml=file soubor s JAR balíčkem s nástrojem A7Soft JExamXML. Je-li parametr
+vynechán uvažuje se implicitní umístění /pub/courses/ipp/jexamxml/jexamxml.jar na serveru Merlin, kde bude test.php hodnocen.
+EOL;
+            exit(0);
+        } 
+        else 
+        {
+        	fwrite(STDERR, "Použi samostatne prepínač '--help' pre zobrazenie nápovedy.\n");
+            exit(10);
+        }
+	}
+
+	foreach ($params as $arg => $option) 
+	{ 
+		if ($arg =='directory')							
+		{
+			global $directory;
+			$tmp_directory = realpath($option);
+			echo $tmp_directory;
+			if ($tmp_directory == false)	
+				exit(11);
+							
+			$directory = $tmp_directory;	
+			echo $directory;	
+		}
+		elseif ($arg =='recursive')
+		{
+			global $recursive_flag;
+			$recursive_flag = true;
+		}
+		elseif ($arg =='parse-script')
+		{
+			global $parse_file;
+			if (is_file($option) == false)
+				exit(11);
+			$parse_file = $option;
+			echo $parse_file;
+		}
+		elseif ($arg =='int-script')
+		{
+			global $int_file;
+			if (is_file($option) == false)
+				exit(11);
+			$int_file = $option;
+		}
+		elseif ($arg =='parse-only')
+		{
+			global $parse_only_flag;
+			$parse_only_flag = true;
+		}
+		elseif ($arg =='int-only')
+		{
+			global $int_only_flag;
+			$int_only_flag = true;
+		}
+		elseif ($arg =='jexamxml')
+		{
+			global $jexamxml_file;
+			if (is_file($option) == false)
+				exit(11);
+			$jexamxml_file = $option;
+		}
+		else
+		{
+			fwrite(STDERR, "Použi prepínač '--help' pre zobrazenie nápovedy.\n");
+	        exit(10);
+		}
+
+		if (array_key_exists('parse-only', $params) and array_key_exists('int-only',$params))
+			fwrite(STDERR, "Chyba! Kombinovanie argumentov --parse-only a --int-only .\n");
+			exit(10);
+	}
+}
+
+
+function TestFiles($source)
+{
+	global $int_file;
+	global $parse_file;
+	global $jexamxml_file;
+
+	global $parseArg,$interpArg;
+	if(substr($source, -2) == ".." || substr($source, -1) == ".")
+	{
+		return False;
+	}
+	if(substr($source, -4) == ".src")
+	{
+		$Name =substr($source, 0,-4);
+		$ref_output = str_replace(".src",".out",$source);
+		$ref_rc = str_replace(".src",".rc",$source);
+		//$in = str_replace(".src",".in",$source);	//zatial nema vyznam ked nemam interpret.py
+
+		if(!file_exists($ref_output))
+		{
+			fwrite(STDERR, "Referenčný output sa nenachádza v priečinku\n");
+			exit(11);
+		}
+		if(!file_exists($ref_rc))
+		{
+			$test_file = fopen($ref_rc, "w");
+			if ($test_file == false)
+			{
+				fwrite(STDERR, "Výstupný súbor nejde otvoriť alebo neexituje!\n");
+				ErrorExit(11);
+			}
+			fwrite($test_file,'0');
+			fclose($test_file);
+		}
+
+		$test_file = fopen($ref_rc, "r");
+		if ($test_file == false)
+		{
+			fwrite(STDERR, "Súbor .rc nie je možné načítať!\n");
+			ErrorExit(11);
+		}
+
+		$exp_rc = fread($test_file,2);
+		$exp_rc = intval($exp_rc);
+		fclose($test_file);
+
+		exec("php3.7 ".$parse_file ." <$source >Temp_output", $result,$rc_out);
+		if($rc_out != $exp_rc)
+		{
+			;//HtmlAdd($Name,False,"Chyba Parseru",0,0);		//rc sa nerovnali
+		}
+		else 
+		{
+			exec("java -jar ".$jexamxml_file." Temp_output ".$ref_output); //otestovat!!!
+			$diff_ret = exec("$?");
+			if($diff_ret == 1)
+		 	{
+				//HtmlAdd($Name,False,"Interpret output match error",0,0);
+			}
+			else
+			{
+				echo "dva subory su rovnake brasko";
+				//HtmlAdd($Name,True,"Diff",0,0);
+			}
+		}
+
+		//TODO - REKURZIA A VOLANIE TEJTO FUNKCIE ,GENEROVANIE HTML -thats all
+
+		unlink("Temp_output");
+	} 
+	return True;	
+}
+
+
+if($recursive_flag == true)
+{
+	$subfolders = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory), RecursiveIteratorIterator::SELF_FIRST);
+	foreach($subfolders as $dir_source => $object) 
+	{
+		TestFiles($dir_source);
+	}
+}
+else
+{
+	foreach (glob($directory."/*.src") as $dir_source) 
+	{
+    	TestFiles($dir_source);
+	}
+}
+
+
+
+
+?>
